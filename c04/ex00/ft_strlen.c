bd2100e1486da4f6a48cd6c24326eb2bd2f796b7
glufy100 <119037482+glufy100@users.noreply.github.com>
2025-07-12 13:26:15 +0200

    add c04 up to ex02

---
+++ c04/ex00/ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 08:14:20 by codespace         #+#    #+#             */
+/*   Updated: 2025/07/12 08:34:31 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/*
+#include <stdio.h>
+int main()
+{
+	char test[] = "salut";
+	printf("%d", ft_strlen(test));
+}
+*/
